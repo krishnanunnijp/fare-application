c272c19db332f2c167fe9bb4bc648c72dc5950be
krishnanunni608 <krishnanunnijpjaya@gmail.com>
2025-05-27 22:28:14 +0530

    Junit test case

--- taxi/taxi/src/test/java/com/road/taxi/TaxiApplicationTests.java
+++ taxi/taxi/src/test/java/com/road/taxi/TaxiApplicationTests.java
@@ -1,14 +1,11 @@
 package com.road.taxi;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 
 import com.road.taxi.dto.RideRequestDto;
 import com.road.taxi.dto.RideResponseDto;
@@ -22,18 +19,20 @@ class TaxiApplicationTests {
 
 	private final FareCalculationService service = new FareCalculationService();
 	@Test
-	void TESTCalsulateFare() {
-		
-		RideRequestDto requestDto = new RideRequestDto();
-		requestDto.setRideId(null);
-		requestDto.setDistance(8.5);
-		requestDto.setDuration(25);
-		requestDto.setTimeOfDay(TimeofDay.PEAK_HOURS);
-		requestDto.setVehicleType(VehicleType.PREMIUM);
-		requestDto.setPassengerLoyaltyTier(LoyalityTiers.GOLD);
-//		ResponseEntity<Object> response =service.calculateFare(requestDto);
-		Exception exception= assertThrows(IllegalArgumentException.class,()-> service.calculateFare(requestDto));
-		assertEquals ("Duration mustbe positive", exception.getMessage());
+	public void testCalculateFare() {
+	    RideRequestDto request = new RideRequestDto();
+	    request.setRideId("R12345");
+	    request.setDistance(8.5);
+	    request.setDuration(25);
+	    request.setTimeOfDay(TimeofDay.PEAK_HOURS);
+	    request.setVehicleType(VehicleType.PREMIUM);
+	    request.setPassengerLoyaltyTier(LoyalityTiers.GOLD);
+
+	    RideResponseDto response = (RideResponseDto) service.calculateFare(request).getBody();
+	    assertNotNull(response);
+	    assertEquals("R12345", response.getRideId());
+	    assertTrue(response.getFinalFare() > 0);
 	}
+
 
 }
